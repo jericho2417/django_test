bff1c7f17b5a4e7c738a0772c4056401750e52b6
jericho2417 <77877485+jericho2417@users.noreply.github.com>
2023-01-16 21:39:29 +0800

    first commit

---
+++ backend/django_project/core/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+
+
+class RegisterAPIView (APIView):
+    def post(self, request):
+        return Response(request.data)
