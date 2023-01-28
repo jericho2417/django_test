ccd69754841f5d35f05f921abb9fc13b720fa8c4
Jericho Marasigan <jerichomarasigan2417@gmail.com>
2023-01-29 00:39:44 +0800

    Adding new files

---
+++ backend/core/views.py
@@ -0,0 +1,44 @@
+from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import exceptions
+from core.authentication import create_access_token
+
+from core.models import User
+from .serializers import UserSerializer
+
+# Create your views here.
+
+
+class RegisterAPIView(APIView):
+    def post(self, request):
+        data = request.data
+        if data['password'] != data['password_confirm']:
+            raise exceptions.APIException("Password doesn't match")
+        serializer = UserSerializer(data=data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data)
+
+
+class LoginAPIView(APIView):
+    def post(self, request):
+        email = request.data['email']
+        password = request.data['password']
+        user = User.objects.filter(email=email).first()
+
+        if user is None:
+            raise exceptions.AuthenticationFailed('Invalid credentials')
+        if not user.check_password(password):
+            raise exceptions.AuthenticationFailed('Invalid credentials')
+        access_token = create_access_token(user.id)
+        refresh_token = create_access_token(user.id)
+        response = Response()
+        response.set_cookie(key='refresh_token',
+                            value=refresh_token, httponly=True)
+        response.data = {
+            'token': access_token
+        }
+        return response
+        # serializer = UserSerializer(user)
+        # return Response(serializer.data)
