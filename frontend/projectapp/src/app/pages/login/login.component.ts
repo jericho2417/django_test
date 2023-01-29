fb22c95437c8d1785a6cac198757b0c76b928ae3
Jericho Marasigan <jerichomarasigan2417@gmail.com>
2023-01-30 03:25:06 +0800

    login api post added to angular http client -jericho Marasigan

--- frontend/projectapp/src/app/pages/login/login.component.ts
+++ frontend/projectapp/src/app/pages/login/login.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { HttpClient } from '@angular/common/http';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -6,5 +9,24 @@ import { Component } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent {
-
+  @ViewChild("myForm")
+  form!: NgForm;
+  constructor(private http: HttpClient, private router: Router) {
+  }
+  onSubmit() {
+    this.http.post('http://127.0.0.1:8000/api/login', {
+      email: this.form.value.email,
+      password: this.form.value.password,
+    }).subscribe({
+      next: (response: any) => {
+        console.log(response.token);
+        this.router.navigate(['/']);
+      },
+      error: (error) => {
+        console.log(error)
+        this.router.navigate(['/login']);
+        ;
+      },
+    });
+  }
 }
