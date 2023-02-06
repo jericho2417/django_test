a15328ab28a59284b29040b75b5fdffae51ec2f8
Jericho Marasigan <jerichomarasigan2417@gmail.com>
2023-02-06 11:49:21 +0800

    delete post -janrie melendrez

--- frontend/projectapp/src/app/pages/post/post.component.ts
+++ frontend/projectapp/src/app/pages/post/post.component.ts
@@ -1,4 +1,4 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Component, ViewChild } from '@angular/core';
 import { NgForm } from '@angular/forms';
 import { ActivatedRoute, Router } from '@angular/router';
@@ -42,10 +42,20 @@ export class PostComponent {
         this.ngOnInit()
       },
       error: (error) => {
-        console.log(error)
-        this.router.navigate(['/login']);
-        ;
+        console.error('There was an error!', error);
       },
     });
+  }
+  delete() {
+    const id = this.route.snapshot.paramMap.get('id');
+    this.http.request('delete', `http://127.0.0.1:8000/api/delete/${id}`)
+      .subscribe({
+        next: data => {
+          this.router.navigate(['/']);
+        },
+        error: error => {
+          console.error('There was an error!', error);
+        }
+      });
   }
 }
